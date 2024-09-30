9ef77f63d9a3eeedffcd2ab5e0cd6cdb8ebbf5b0
Jose David Soto Puac <jsoto-2019315@kinal.edu.gt>
2024-09-30 17:57:29 -0600

    Login form design

--- LoginApp/src/app/app-routing.module.ts
+++ LoginApp/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () =>
-      import('./auth/authh.module').then((m) => m.AUTH_MODULE),
+    component: LoginComponent
   },
 ];
 
